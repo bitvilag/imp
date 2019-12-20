b225aa0fad333a156316f0105726f1b373e06995
Bakos Attila <bitvilag@gmail.com>
2019-12-20 22:18:45 +0100

    a

--- io/multi.c
+++ io/multi.c
@@ -9,7 +9,7 @@ int main() {
   int j; /* loop counter for inner loop */
   for (i = 1; i <= 10; ++i) { /* outer for loop */
     for (j = 1; j <= 10; ++j) { /* nested for loop (inner loop) */
-      printf("%2d x %2d = %2d, ", i, j, i*j); /* format specifier with width */
+      printf("%.2d x %.2d = %.2d, ", i, j, i*j); /* format specifier with width */
     }
     printf("\n");
   }
